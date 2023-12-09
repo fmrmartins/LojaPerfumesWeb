a97f6ee3a26249db3456bcdc224c9ab347dd06f9
fmrmartins <137707904+fmrmartins@users.noreply.github.com>
2023-12-09 13:14:29 -0300

    Projeto Inicial

---
+++ src/main/java/com/perfumes/PerfuWeb/PerfuWebApplication.java
@@ -0,0 +1,13 @@
+package com.perfumes.PerfuWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfuWebApplication.class, args);
+	}
+
+}
